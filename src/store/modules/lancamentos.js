85c497e81b0cc03af9b391fd50186398578dcf38
Leonardo de Faveri <83522757+Leodf@users.noreply.github.com>
2022-09-23 09:33:55 -0300

    incluindo o vuex para manipulação global dos dados no vue

---
+++ src/store/modules/lancamentos.js
@@ -0,0 +1,33 @@
+const moduloLancamentos = {
+    state: {
+        lancamentos: [
+            {
+                id: Math.random().toString(36).substring(2, 5),
+                valor: -50,
+                descricao: 'Compras no mercado',
+                data: '2022-09-23'
+            },
+            {
+                id: Math.random().toString(36).substring(2, 5),
+                valor: 100,
+                descricao: 'Vendas de HQ',
+                data: '2022-08-25'
+            },
+            {
+                id: Math.random().toString(36).substring(2, 5),
+                valor: -25,
+                descricao: 'Farmácia',
+                data: '2022-09-20'
+            },
+        ],
+        caixa: 0
+    },
+    getters: {
+        todosLancamentos: state => state.lancamentos,
+        dinheiroEmCaixa: state => state.caixa
+    },
+    actions: {},
+    mutations: {}
+}
+
+export default moduloLancamentos
